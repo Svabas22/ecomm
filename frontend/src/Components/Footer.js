a4d0aa8c16cea93b1fcd78a70f00f5e72440ab64
rytuks1337 <112757266+rytuks1337@users.noreply.github.com>
2023-11-25 19:25:02 +0200

    Update fallen files

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -10,22 +10,34 @@ function Footer() {
   };
   return (
     <>
-      <div className="footer">
-        <div className="footer-content">
-          <div className="footer-col">
-            <div className="footer-info">Informacija</div>
-            <div className="footer-btn">Apie</div>
-          </div>
-          <div className="footer-col">
-            <div className="footer-btn">
-              <a href="mailto:">
-                <h2>Kontaktai</h2>
-              </a>
+      <div className="footer-wrapper">
+        <div className="footer">
+          <div className="footer-row">
+            <div className="footer-col">
+              <div className="footer-info">Informacija</div>
+              <div className="footer-btn">
+                <a href="http://localhost:3000/about" target="_blank">
+                  Apie
+                </a>
+              </div>
+            </div>
+            <div className="footer-col">
+              <div className="footer-btn" onClick={goToTop}>
+                <h2>To top</h2>
+              </div>
+            </div>
+            <div className="footer-col">
+              <div className="footer-info">Susisiekti</div>
+              <div className="footer-btn">
+                <a href="mailto:">Kontaktai</a>
+              </div>
             </div>
           </div>
-          <div className="footer-col">
-            <div className="footer-btn" onClick={goToTop}>
-              <h2>To top</h2>
+          <div className="footer-row">
+            <div className="footer-col">
+              <div className="footer-btn">
+                <Link to="/home">Home</Link>
+              </div>
             </div>
           </div>
         </div>
