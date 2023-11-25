f240f2175ae0274f4baa55f1415c959dd2aa6077
rytuks1337 <112757266+rytuks1337@users.noreply.github.com>
2023-11-25 02:34:17 +0200

    Backend

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+const dotenv = require('dotenv')
+
+dotenv.config();
+const app = express()
+app.use(cors())
+
+const db = mysql.createConnection({
+    host: 'database',
+    user: 'user',
+    password: process.env.CONNECTION_PASS,
+    database: 'mysql'
+})
+
+app.get('/',(re,res)=>{
+return res.json("Backend");
+
+})
+
+app.get('/users',(rreq,res)=>{
+    const sql = "SELECT * FROM users";
+    db.query(sql,(err,data)=>{
+        if(err) return res.json(err);
+        return res.json(data);
+    })
+})
+
+app.listen(1234,()=>{
+    console.log("listening");
+})
