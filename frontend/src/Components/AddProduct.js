ddcd34268d36ca4d90dfd4e513617d730a88a912
KwertyKeru <ugnius844@gmail.com>
2023-11-30 19:43:21 +0200

    Fixed whitespace when resizing in "Home", "Login, "Register" and "AddProduct" pages. To do left: "Buy", "About".

--- frontend/src/Components/AddProduct.js
+++ frontend/src/Components/AddProduct.js
@@ -5,18 +5,16 @@ function AddProduct() {
   TabTitle("Add Product");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
-  const [tier, setTier] = useState("");
-  const [division, setDivision] = useState("");
+  const [region, setRegion] = useState("");
   const [price, setPrice] = useState("");
   async function addProduct() {
-    console.warn(name, password, tier, division, price);
+    console.warn(name, password, region, price);
     const formData = new FormData();
     formData.append("name");
     formData.append("password");
-    formData.append("tier");
-    formData.append("division");
+    formData.append("region");
     formData.append("price");
-    let result = await fetch("http://localhost:8000/api/addaccount?=", {
+    let result = await fetch("http://localhost:8000/api/add", {
       method: "POST",
       body: formData,
     });
@@ -27,53 +25,54 @@ function AddProduct() {
       <div className="main">
         <Header />
         <div className="body">
-          <div className="wrapper-addAccount"></div>
-          <div className="addAccountBackground"></div>
-          <div className="addAccount-content">
-            <div className="col-sm-6 offset-sm-3">
-              <div className="Page-title">
-                <h1>Add account</h1>
+          <div className="wrapper-addProduct">
+            {" "}
+            <div className="addProductBackground"></div>
+            <div className="addProduct-content">
+              <div className="col-sm-6 offset-sm-3">
+                <div className="Page-title">
+                  <h1>Add account</h1>
+                </div>
+                <div className="row-input">
+                  <p>Username</p>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="name"
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
+                <div className="row-input">
+                  <p>Password</p>
+                  <input
+                    type="password"
+                    className="form-control"
+                    placeholder="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+                <div className="row-input">
+                  <p>Region</p>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="region"
+                    onChange={(e) => setRegion(e.target.value)}
+                  />
+                </div>
+                <div className="row-input">
+                  <p>Price</p>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="price"
+                    onChange={(e) => setPrice(e.target.value)}
+                  />
+                </div>
+                <button onClick={addProduct} className="btn btn-primary">
+                  Add Product
+                </button>
               </div>
-              <div className="row-input">
-                <p>Username</p>
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="name"
-                  onChange={(e) => setName(e.target.value)}
-                />
-              </div>
-              <div className="row-input">
-                <p>Password</p>
-                <input
-                  type="password"
-                  className="form-control"
-                  placeholder="password"
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
-              <div className="row-input">
-                <p>Region</p>
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="region"
-                  onChange={(e) => setTier(e.target.value)}
-                />
-              </div>
-              <div className="row-input">
-                <p>Price</p>
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="price"
-                  onChange={(e) => setPrice(e.target.value)}
-                />
-              </div>
-
-              <button onClick={addProduct} className="btn btn-primary">
-                Add Product
-              </button>
             </div>
           </div>
         </div>
