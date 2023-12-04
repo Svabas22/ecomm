fc62a9c5a14d96b6d30243ecc9412aac34e9ab08
KwertyKeru <ugnius844@gmail.com>
2023-12-04 23:05:20 +0200

    useEffect

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -5,22 +5,19 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { json } from "react-router-dom";
 
-function Home() {
-  const [gridData, setGridData] = useState(null);
-
-  useEffect(() => {
-    const fetching = async () => {
-      try {
-        const data = await axios.get("/api/listings").then((response) => {
-          return response.data;
-        });
-        setGridData(data[0]);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-    fetching();
-  }, []);
+useEffect(() => {
+  const fetching = async () => {
+    try {
+      const data = await axios.get("/api/listings").then((response) => {
+        return response.data;
+      });
+      setGridData(data); // Set the entire array, not just the first element
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+  fetching();
+}, []);
 
   TabTitle("Home");
   return (
