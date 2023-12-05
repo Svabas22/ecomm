b08ce4cb0f6c5da273c88358e76cb797d791bb22
rytuks1337 <112757266+rytuks1337@users.noreply.github.com>
2023-12-05 22:20:41 +0200

    hfh

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -52,7 +52,7 @@ function Home() {
               <div
                 key={index}
                 className="grid"
-                onClick={redirect(item.list_Name,item.list_region,item.list_price,)}
+                onClick={redirect(item.list_Name,item.list_region,item.list_price)}
               >
                 <div className="row-grid">
                   <img className="grid-icon" src={worldIcon} />
