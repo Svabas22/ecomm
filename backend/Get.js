f8595e1d290450a588c99e3305027dde85789a69
rytuks1337 <112757266+rytuks1337@users.noreply.github.com>
2023-12-05 13:20:31 +0200

    testing

--- backend/Get.js
+++ backend/Get.js
@@ -7,6 +7,10 @@ const bcrypt = require("bcrypt");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
+const path = require('path');
+
+
+
 dotenv.config();
 
 const app = express();
@@ -15,6 +19,13 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
+
 
 function validate_input(inputString) {
   const specialChars = [
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,13 +5,19 @@ import App from "./App.js";
 import reportWebVitals from "./reportWebVitals.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
