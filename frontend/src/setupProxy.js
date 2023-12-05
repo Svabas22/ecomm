ec89fd3d27878a67157400158feba172d77c3417
rytuks1337 <112757266+rytuks1337@users.noreply.github.com>
2023-12-05 03:27:51 +0200

    updating

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:4321', // Replace with your backend URL
+      target: 'http://vhost1.localhost:4321', // Replace with your backend URL
       changeOrigin: true,
     })
   );
