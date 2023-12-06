d75ba197c2e8c3a6c6297117daa4e15425b7c35a
KwertyKeru <ugnius844@gmail.com>
2023-12-06 14:20:56 +0200

    test

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -41,47 +41,49 @@ function Home() {
       <div className="main">
         <Header />
         <div className="body">
-          <div className="search">
-            <input className="search-input" placeholder="Search"></input>
-            <div className="search-input-icon">
-              <img src={search_logo}></img>
-            </div>
-          </div>
           {gridData.length === 0 ? (
             <h1>List is empty.</h1>
           ) : (
-            <div className="grid-wrapper">
-              {gridData.map((item, index) => (
-                <div
-                  key={index}
-                  className="grid"
-                  onClick={() =>
-                    redirect(
-                      item.list_id,
-                      item.list_Name,
-                      item.list_region,
-                      item.list_price
-                    )
-                  }
-                >
-                  <div className="row-grid">
-                    <img className="grid-icon" src={worldIcon} />
-                    <div className="grid-title">
-                      <h6>{item.list_Name}</h6>
-                    </div>
-                  </div>
-                  <hr />
-                  <div className="row-grid account">
-                    <div className="tier-level">{item.list_region}</div>
-                    <div className="grid-price">
-                      <div className="price-text">Price</div>
-                      <div className="price-num">{item.list_price}</div>
-                      <div className="price-currency">EUR</div>
-                    </div>
-                  </div>
+            <>
+              <div className="search">
+                <input className="search-input" placeholder="Search"></input>
+                <div className="search-input-icon">
+                  <img src={search_logo}></img>
                 </div>
-              ))}
-            </div>
+              </div>
+              <div className="grid-wrapper">
+                {gridData.map((item, index) => (
+                  <div
+                    key={index}
+                    className="grid"
+                    onClick={() =>
+                      redirect(
+                        item.list_id,
+                        item.list_Name,
+                        item.list_region,
+                        item.list_price
+                      )
+                    }
+                  >
+                    <div className="row-grid">
+                      <img className="grid-icon" src={worldIcon} />
+                      <div className="grid-title">
+                        <h6>{item.list_Name}</h6>
+                      </div>
+                    </div>
+                    <hr />
+                    <div className="row-grid account">
+                      <div className="tier-level">{item.list_region}</div>
+                      <div className="grid-price">
+                        <div className="price-text">Price</div>
+                        <div className="price-num">{item.list_price}</div>
+                        <div className="price-currency">EUR</div>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </>
           )}
         </div>
       </div>
