80d056b4d68aa38423daa56e58cb2714e11db604
KwertyKeru <ugnius844@gmail.com>
2023-12-06 11:05:45 +0200

    Added useState, worldIcon, changed 'result' to 'data'

--- frontend/src/Components/AccountList.js
+++ frontend/src/Components/AccountList.js
@@ -1,16 +1,16 @@
 import { TabTitle } from "../Utilities/TabTitle.js";
 import Header from "./Header.js";
 import axios from "axios";
+import { useState } from "react";
+import worldIcon from "../Images/world_icon.jpg";
 
 function AccountList() {
-  const navigate = useNavigate();
   const [gridData, setGridData] = useState([]);
-  async function removeelisting(){
-    let result = await axios.get("/api/listuser").then((response) => {
+  async function removeelisting() {
+    let data = await axios.get("/api/listuser").then((response) => {
       return response.data;
     });
-    setGridData(data)
-    
+    setGridData(data);
   }
   TabTitle("Account List");
 
@@ -20,31 +20,29 @@ function AccountList() {
         <Header />
         <div className="body">
           <div className="grid-wrapper">
-              {gridData.map((item, index) => (
-                <div
-                  key={index}
-                  className="grid"
-                  onClick={() =>
-                    removeelisting()
-                  }
-                >
-                  <div className="row-grid">
-                    <img className="grid-icon" src={worldIcon} />
-                    <div className="grid-title">
-                      <h6>{item.list_Name}</h6>
-                    </div>
-                  </div>
-                  <hr />
-                  <div className="row-grid account">
-                    <div className="tier-level">{item.list_region}</div>
-                    <div className="grid-price">
-                      <div className="price-text">Price</div>
-                      <div className="price-num">{item.list_price}</div>
-                      <div className="price-currency">EUR</div>
-                    </div>
+            {gridData.map((item, index) => (
+              <div
+                key={index}
+                className="grid"
+                onClick={() => removeelisting()}
+              >
+                <div className="row-grid">
+                  <img className="grid-icon" src={worldIcon} />
+                  <div className="grid-title">
+                    <h6>{item.list_Name}</h6>
                   </div>
                 </div>
-              ))}
+                <hr />
+                <div className="row-grid account">
+                  <div className="tier-level">{item.list_region}</div>
+                  <div className="grid-price">
+                    <div className="price-text">Price</div>
+                    <div className="price-num">{item.list_price}</div>
+                    <div className="price-currency">EUR</div>
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
