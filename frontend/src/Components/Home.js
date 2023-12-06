d48082e4775ff7f2cb320fdfe23e2dff7026530b
KwertyKeru <ugnius844@gmail.com>
2023-12-06 14:03:28 +0200

    Home style

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -48,36 +48,40 @@ function Home() {
             </div>
           </div>
           <div className="grid-wrapper">
-            {gridData.map((item, index) => (
-              <div
-                key={index}
-                className="grid"
-                onClick={() =>
-                  redirect(
-                    item.list_id,
-                    item.list_Name,
-                    item.list_region,
-                    item.list_price
-                  )
-                }
-              >
-                <div className="row-grid">
-                  <img className="grid-icon" src={worldIcon} />
-                  <div className="grid-title">
-                    <h6>{item.list_Name}</h6>
+            {gridData.length === 0 ? (
+              <h1>List is empty.</h1>
+            ) : (
+              gridData.map((item, index) => (
+                <div
+                  key={index}
+                  className="grid"
+                  onClick={() =>
+                    redirect(
+                      item.list_id,
+                      item.list_Name,
+                      item.list_region,
+                      item.list_price
+                    )
+                  }
+                >
+                  <div className="row-grid">
+                    <img className="grid-icon" src={worldIcon} />
+                    <div className="grid-title">
+                      <h6>{item.list_Name}</h6>
+                    </div>
+                  </div>
+                  <hr />
+                  <div className="row-grid account">
+                    <div className="tier-level">{item.list_region}</div>
+                    <div className="grid-price">
+                      <div className="price-text">Price</div>
+                      <div className="price-num">{item.list_price}</div>
+                      <div className="price-currency">EUR</div>
+                    </div>
                   </div>
                 </div>
-                <hr />
-                <div className="row-grid account">
-                  <div className="tier-level">{item.list_region}</div>
-                  <div className="grid-price">
-                    <div className="price-text">Price</div>
-                    <div className="price-num">{item.list_price}</div>
-                    <div className="price-currency">EUR</div>
-                  </div>
-                </div>
-              </div>
-            ))}
+              ))
+            )}
           </div>
         </div>
       </div>
