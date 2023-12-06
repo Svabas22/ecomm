e706c2c0d29cfc7bcc4ad4425d4eac63d8387871
rytuks1337 <112757266+rytuks1337@users.noreply.github.com>
2023-12-06 10:04:45 +0200

    a

--- frontend/src/Components/AccountList.js
+++ frontend/src/Components/AccountList.js
@@ -3,17 +3,43 @@ import Header from "./Header.js";
 
 function AccountList() {
   TabTitle("Account List");
+  
   return (
     <>
       <div className="main">
         <Header />
         <div className="body">
-          <div className="user-name">
-            <h1>Accounts</h1>
-          </div>
-          <div className="user-list">
-            <h2>List</h2>
-          </div>
+        <div className="grid-wrapper">
+            {gridData.map((item, index) => (
+              <div
+                key={index}
+                className="grid"
+                onClick={() =>
+                  redirect(
+                    item.list_id,
+                    item.list_Name,
+                    item.list_region,
+                    item.list_price
+                  )
+                }
+              >
+                <div className="row-grid">
+                  <img className="grid-icon" src={worldIcon} />
+                  <div className="grid-title">
+                    <h6>{item.list_Name}</h6>
+                  </div>
+                </div>
+                <hr />
+                <div className="row-grid account">
+                  <div className="tier-level">{item.list_region}</div>
+                  <div className="grid-price">
+                    <div className="price-text">Price</div>
+                    <div className="price-num">{item.list_price}</div>
+                    <div className="price-currency">EUR</div>
+                  </div>
+                </div>
+              </div>
+            ))}
         </div>
       </div>
     </>
